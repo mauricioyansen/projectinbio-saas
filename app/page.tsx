5be61e6aaa2b8edd1f084217414c07f5dedef6e1
Mauricio <mauricio_cy@hotmail.com>
2025-03-15 13:45:45 -0300

    header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { Header } from "./components/landing-page/Header";
 import { Hero } from "./components/landing-page/Hero";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
-      {/* <Header/> */}
+      <Header />
       <Hero />
       {/* <VideoExplanation/> */}
       {/* <Pricing/> */}
