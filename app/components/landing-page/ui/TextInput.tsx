e397226b95c866d643fd3a6fdfc2114ca6c5e5af
Mauricio <mauricio_cy@hotmail.com>
2025-03-15 01:14:41 -0300

    input component

---
+++ app/components/landing-page/ui/TextInput.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/app/lib/utils";
+
+export function TextInput(props: React.InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input
+      {...props}
+      className={cn(
+        `
+    w-full p-3 bg-[var(--background-secondary)] text-white placeholder:text-[var(--content-placeholder)] rounded-xl border border-transparent hover:border-[var(--border-secondary)] hover:text-[var(--content-body)] active:border-[var(--border-tertiary)]`,
+        props.className
+      )}
+    />
+  );
+}
